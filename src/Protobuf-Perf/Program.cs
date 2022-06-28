cf10070a609c0b4dca07211b6b2abaa1fd498e71
WernerMairl <werner.mairl@gmail.com>
2022-06-28 08:45:54 +0200

    refine tests

--- src/Protobuf-Perf/Program.cs
+++ src/Protobuf-Perf/Program.cs
@@ -15,10 +15,19 @@ namespace ConsoleApp
 {
     public partial class Program
     {
+        private const bool SkipState = true;
+        private const bool SkipNet = true;
+        private const bool SkipUTF8 = false;
         private const bool SkipGoogle = true;
-        private const bool SkipOthers = false;
+        private const bool SkipWarmup = false;
 
-        private static string ToMeasureString(Stopwatch watch, long operations, int padleft = 14)
+
+        /// <summary>
+        /// means dummy and construction
+        /// </summary>
+        private const bool SkipOthers = true;
+
+        private static string ToMeasureString(Stopwatch watch, long operations, int padleft = 15)
         {
             // watch.ElapsedMilliseconds.ToString() + "ms"
             double opsPerSecond = operations / watch.Elapsed.TotalSeconds / 1000;
@@ -31,14 +40,15 @@ namespace ConsoleApp
         }
 
         public static bool ThreadLocalTypeModel { get; set; } = true;
-        public static bool IsLockFree { get; set; } = false;
+        public static bool IsLockFree { get; set; } = true;
 
         public static int ObjectsForTesting { get; set; } = 2000000;
 
         public static bool InitializeInstance { get; set; } = true;
+
         private static void Main(string[] args)
         {
-            Console.WriteLine("Protobuf-Net Performance Investigations v1.5.2");
+            Console.WriteLine("Protobuf-Net Performance Investigations v1.5.3-manualLoop01");
             Console.WriteLine();
             foreach (string s in args)
             {
@@ -57,7 +67,7 @@ namespace ConsoleApp
 
                     if (string.Equals(splits[0], "Objects", StringComparison.OrdinalIgnoreCase))
                     {
-                        ObjectsForTesting = int.Parse(splits[1])*1000;
+                        ObjectsForTesting = int.Parse(splits[1]) * 1000;
                     }
 
                     if (string.Equals(splits[0], "IsLockFree", StringComparison.OrdinalIgnoreCase))
@@ -76,6 +86,22 @@ namespace ConsoleApp
             Console.WriteLine($"  IsLockFree={IsLockFree}");
             Console.WriteLine($"  InitializeInstance={InitializeInstance}");
             Console.WriteLine();
+
+            //UTF Preparations
+
+            var Utf8TestData = new List<byte[]>();
+
+            if (!SkipUTF8)
+            {
+                for (int i = 0; i < ObjectsForTesting; i++)
+                {
+                    using (var ms = new MemoryStream())
+                    {
+                        var bytes = UTF8.Value.GetBytes("some text " + i.ToString());
+                        Utf8TestData.Add(bytes);
+                    }
+                }
+            }
 
             //protobuf-net preparations
 
@@ -103,18 +129,21 @@ namespace ConsoleApp
                     }
                 }
             }
-            var warmupOutput = new Test[ObjectsForTesting];
 
-            //Warmup ProtoBuf-Net (without measuring)
-            protobufNetTestData.AsParallel().WithExecutionMode(ParallelExecutionMode.ForceParallelism).Select((x, i) =>
+            if (!SkipWarmup)
             {
-                warmupOutput[i] = NetDeserialize<Test>(x, InitializeInstance ? new Test(): null);
-                return true;
-            }).All(_ => _);
+                var warmupOutput = new Test[ObjectsForTesting];
 
+                //Warmup ProtoBuf-Net (without measuring)
+                protobufNetTestData.AsParallel().WithExecutionMode(ParallelExecutionMode.ForceParallelism).Select((x, i) =>
+                {
+                    warmupOutput[i] = NetDeserialize<Test>(x, InitializeInstance ? new Test() : null);
+                    return true;
+                }).All(_ => _);
+            }
             var output = new Test[ObjectsForTesting];
 
-            int firstMeasureColumn = 70;
+            int firstMeasureColumn = 75;
             //Start measurements for ProtoBuf-Net
 
             int[] degreeOfParallelism = new int[] { 2, 4, 6, 8, 10, 12 };
@@ -126,50 +155,122 @@ namespace ConsoleApp
 
             var watch = new Stopwatch();
             int index = 0;
-            Console.Write("NET: without AsParallel (Deserialize):".PadRight(firstMeasureColumn));
-            watch.Restart();
-            foreach (var x in protobufNetTestData)
+            if (!SkipNet)
             {
-                output[index++] = NetDeserialize<Test>(x, InitializeInstance ? new Test() : null);
-            }
-            Console.WriteLine(ToMeasureString(watch, ObjectsForTesting));
-
-            foreach (int parallelism in degreeOfParallelism)
-            {
-                Console.Write($"   NET: with AsParallel (Deserialize with DegreeOfParallelism={parallelism}):".PadRight(firstMeasureColumn));
+                Console.Write("NET: without AsParallel (Deserialize):".PadRight(firstMeasureColumn));
                 watch.Restart();
-                protobufNetTestData.AsParallel().WithExecutionMode(ParallelExecutionMode.ForceParallelism).WithDegreeOfParallelism(parallelism).Select((x, i) =>
+                foreach (var x in protobufNetTestData)
+                {
+                    output[index++] = NetDeserialize<Test>(x, InitializeInstance ? new Test() : null);
+                }
+                Console.WriteLine(ToMeasureString(watch, ObjectsForTesting));
+
+                foreach (int parallelism in degreeOfParallelism)
+                {
+                    Console.Write($"   NET: with AsParallel (Deserialize with DegreeOfParallelism={parallelism}):".PadRight(firstMeasureColumn));
+                    watch.Restart();
+                    protobufNetTestData.AsParallel().WithExecutionMode(ParallelExecutionMode.ForceParallelism).WithDegreeOfParallelism(parallelism).Select((x, i) =>
+                    {
+                        output[i] = NetDeserialize<Test>(x, InitializeInstance ? new Test() : null);
+                        return true;
+                    }).All(_ => _);
+                    Console.WriteLine(ToMeasureString(watch, ObjectsForTesting));
+                }
+
+                Console.Write($"   NET: with AsParallel (Deserialize with DegreeOfP.=default):".PadRight(firstMeasureColumn));
+                watch.Restart();
+                protobufNetTestData.AsParallel().WithExecutionMode(ParallelExecutionMode.ForceParallelism).Select((x, i) =>
                 {
                     output[i] = NetDeserialize<Test>(x, InitializeInstance ? new Test() : null);
                     return true;
                 }).All(_ => _);
                 Console.WriteLine(ToMeasureString(watch, ObjectsForTesting));
+
+                Console.Write("   NET: with Parallel.ForEach (Deserialize):".PadRight(firstMeasureColumn));
+                watch.Restart();
+                Parallel.ForEach(protobufNetTestData, () => 0, (x, pls, index, s) =>
+                {
+                    output[(int)index] = NetDeserialize<Test>(x, InitializeInstance ? new Test() : null);
+                    return 0;
+                }, _ => { });
+                Console.WriteLine(ToMeasureString(watch, ObjectsForTesting));
+            }
+            if (!SkipState)
+            {
+                watch.Restart();
+                Console.Write($"   State: NO-OP with AsParallel:".PadRight(firstMeasureColumn));
+                protobufNetTestData.AsParallel().WithExecutionMode(ParallelExecutionMode.ForceParallelism).Select((x, i) =>
+                {
+                    var m = StateDeserialize<Test>(x, true, InitializeInstance ? new Test() : null);
+                    return true;
+                }).All(_ => _);
+                Console.WriteLine(ToMeasureString(watch, ObjectsForTesting));
+
+                watch.Restart();
+                Console.Write($"   State: with AsParallel:".PadRight(firstMeasureColumn));
+                protobufNetTestData.AsParallel().WithExecutionMode(ParallelExecutionMode.ForceParallelism).Select((x, i) =>
+                {
+                    var m = StateDeserialize<Test>(x, false, InitializeInstance ? new Test() : null);
+                    return true;
+                }).All(_ => _);
+                Console.WriteLine(ToMeasureString(watch, ObjectsForTesting));
             }
 
-            Console.Write($"   NET: with AsParallel (Deserialize with DegreeOfP.=default):".PadRight(firstMeasureColumn));
-            watch.Restart();
-            protobufNetTestData.AsParallel().WithExecutionMode(ParallelExecutionMode.ForceParallelism).Select((x, i) =>
+            if (!SkipUTF8)
             {
-                output[i] = NetDeserialize<Test>(x, InitializeInstance ? new Test() : null);
-                return true;
-            }).All(_ => _);
-            Console.WriteLine(ToMeasureString(watch, ObjectsForTesting));
+                System.Threading.Thread.Sleep(3000);
 
-            Console.Write("   NET: with Parallel.ForEach (Deserialize):".PadRight(firstMeasureColumn));
-            watch.Restart();
-            Parallel.ForEach(protobufNetTestData, () => 0, (x, pls, index, s) =>
-            {
-                output[(int)index] = NetDeserialize<Test>(x, InitializeInstance ? new Test() : null);
-                return 0;
-            }, _ => { });
-            Console.WriteLine(ToMeasureString(watch, ObjectsForTesting));
+                watch.Restart();
+
+
+                Console.Write($"   UTF8: NOT Parallel:".PadRight(firstMeasureColumn));
+                for (int ii = 0; ii < ObjectsForTesting; ii++)
+                {
+                    var x = Utf8TestData[ii];
+                    var m = Utf8Deserialize<Test>(x, false, InitializeInstance ? new Test() : null);
+                }
+
+                //Utf8TestData.Select((x, i) =>
+                //{
+                //    var m = Utf8Deserialize<Test>(x, false, InitializeInstance ? new Test() : null);
+                //    return true;
+                //}).All(_ => _);
+
+                Console.WriteLine(ToMeasureString(watch, ObjectsForTesting));
+
+                //            ReadOnlySpan<byte> span = new ReadOnlySpan<byte>(buffer);
+
+                watch.Restart();
+                Console.Write($"   UTF8-SPAN: NOT Parallel:".PadRight(firstMeasureColumn));
+                var theSpan = new ReadOnlySpan<byte>(Utf8TestData.First());
+                ReadOnlySpan<byte> span = new ReadOnlySpan<byte>(Utf8TestData.First());
+
+                for (int iii = 0; iii < ObjectsForTesting; iii++)
+                {
+                    var m = Utf8SpanDeserialize<Test>(span, InitializeInstance ? new Test() : null);
+
+                }
+
+                //Utf8TestData.Select((x, i) =>
+                //{
+                //    var m = Utf8SpanDeserialize<Test>(span, InitializeInstance ? new Test() : null);
+                //    return true;
+                //}).All(_ => _);
+                Console.WriteLine(ToMeasureString(watch, ObjectsForTesting));
+
+
+
+
+
+            }
 
             if (!SkipGoogle)
             {
+                var googleOutput = new Test[ObjectsForTesting];
                 //Warmup Google.Protobuf (without measuring)
                 googleProtobufTestData.AsParallel().WithExecutionMode(ParallelExecutionMode.ForceParallelism).Select((x, i) =>
                 {
-                    warmupOutput[i] = GoogleDeserialize<Test>(x);
+                    googleOutput[i] = GoogleDeserialize<Test>(x);
                     return true;
                 }).All(_ => _);
 
@@ -268,7 +369,7 @@ namespace ConsoleApp
             RuntimeTypeModel rt;
             if (ThreadLocalTypeModel)
             {
-                rt = RuntimeTypeModel.Create($"ThreadID={Environment.CurrentManagedThreadId}", lockFree:IsLockFree);
+                rt = RuntimeTypeModel.Create($"ThreadID={Environment.CurrentManagedThreadId}", lockFree: IsLockFree);
                 rt.Add(typeof(Test), true);
             }
             else
@@ -276,14 +377,65 @@ namespace ConsoleApp
                 rt = RuntimeTypeModel.Default;
             }
             rt[typeof(Test)].CompileInPlace();
-            return rt;
+            //rt[typeof(Test)].SerializerType;
+            //var options = new RuntimeTypeModel.CompilerOptions();
+            //options.OutputPath = Directory.GetCurrentDirectory();
+            //var dll = rt.Compile(options);
+            ////var model = ThreadRTM.Value;
+            ////_dll = model.Compile("MySerializer", "DalSerializer.dll");
+            //Console.WriteLine("Serializer written to " + Directory.GetCurrentDirectory());
 
+            return rt;
         });
 
         private static T NetDeserialize<T>(byte[] buffer, T initvalue)
         {
             var model = ThreadRTM.Value; //RuntimeTypeModel.Default
-            return model.Deserialize<T>((ReadOnlySpan<byte>)buffer, initvalue, null);
+            return model.Deserialize<T>((ReadOnlySpan<byte>)buffer, initvalue, typeof(T));
+        }
+
+        private static ThreadLocal<System.Text.UTF8Encoding> UTF8 = new ThreadLocal<System.Text.UTF8Encoding>(() =>
+        {
+            return new System.Text.UTF8Encoding();
+        });
+
+        private static T Utf8SpanDeserialize<T>(ReadOnlySpan<byte> span, T initvalue)
+        {
+            var s = UTF8.Value.GetString(span);
+            return initvalue;
+        }
+
+        private static T Utf8Deserialize<T>(byte[] buffer, bool noOp, T initvalue)
+        {
+            var s = UTF8.Value.GetString(buffer, 0, buffer.Length);
+            return initvalue;
+        }
+
+        private static T StateDeserialize<T>(byte[] buffer, bool noOp, T initvalue)
+        {
+            var model = ThreadRTM.Value;
+            using (var state = ProtoReader.State.Create(buffer, model))
+            {
+                if (!noOp)
+                {
+                    int fieldNumber;
+                    int loops = 0;
+                    while ((fieldNumber = state.ReadFieldHeader()) > 0)
+                    {
+                        loops += 1;
+                        if (state.WireType == WireType.String)
+                        {
+                            state.ReadString();
+                        }
+                        else
+                        {
+                            Debug.Fail("not in this sample");
+                        };
+                    }
+                    Debug.Assert(loops == 2);
+                }
+            }
+            return initvalue;
         }
 
         private static Test GoogleDeserialize<T>(byte[] buffer)
